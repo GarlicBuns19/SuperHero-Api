22c88cb39ce9d45ab349ec2460c65928b1d0ea77
GarlicBuns19 <planterx26@gmail.com>
2022-07-18 19:25:02 +0100

    Added colors to web

--- superHero.js
+++ superHero.js
@@ -1,25 +1,23 @@
 superHeroUrl = "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/";
 
-const row = document.querySelector(".row");
-
-function heroLayout() {
-  row.innerHTML = `
- <h1></h1>
-        <div class="col-6" id="heroImg"></div>
-        <div class="col-6" id="heroData"></div>
- `;
-}
+const container = document.querySelector(".container");
 
 async function displayHero(url) {
   let data = await fetch(url + "all.json")
     .then((res) => res.json())
     .then((data) =>
       data.forEach((hero) => {
-        row.innerHTML += `<button>${hero.id} ${hero.name}</button>`
-        console.log(hero.id);
-
-        // console.log(hero.name);
-        // console.log(hero["images"].lg);
+        container.innerHTML += `
+        <div class="row">
+          <div class="col-6">
+            <img src="${hero.images.sm}" alt="" class="img-fluid ${hero.appearance.gender}" id="heroImg">
+          </div>
+          <div class="col-6" id="heroData">
+            <h1>${hero.name}</h1>
+            <h2 class="${hero.appearance.gender}">${hero.appearance.gender}</h2>
+          </div>
+        </div>
+        `;
       })
     );
   try {
