0e0c9c755d727490f9a8cb70ea9be342d6737f1b
GarlicBuns19 <planterx26@gmail.com>
2022-07-18 05:55:24 +0100

    Accessed the data

---
+++ superHero.js
@@ -0,0 +1,22 @@
+superHeroUrl = ('https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/')
+
+function heroLayout(){
+
+};
+
+async function displayHero(url){
+    let data =  await fetch(url + 'all.json')
+    .then(res => res.json())
+    .then(data => data.forEach((hero)=>{
+        console.log(hero.id)
+        console.log(hero.name   )
+    }))
+    try{
+        console.log('Success')
+    }
+    catch(error){
+        console.log(error)
+    }
+    return data
+}
+displayHero(superHeroUrl)
